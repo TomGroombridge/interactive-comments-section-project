babb4ad13ffb3c6ca7f51ea7236bcd52892dfdbb
Tom Groombridge <tom.groombridge@zopa.com>
2022-05-30 20:04:40 +0200

    fetch comments data from api and handle state

--- src/pages/Comments/index.jsx
+++ src/pages/Comments/index.jsx
@@ -1,17 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Comments = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    // lets fetch those comments
+    fetchComments();
     // let's not forget to use state for the Promise values (data, loading, error)
   }, [])
 
+  const fetchComments = async () => {
+    try {
+      const response = await fetch("https://api.mocki.io/v2/a20ae30b/comments")
+      
+      const data = await response.json();
+      setComments(data.comments)
+      setLoading(false)
+      console.log(comments)
+
+    }
+    catch (error) {
+      setError(true)
+    }
+  }
+
+  if (loading) return <h1>Loading...</h1>
+  
+  if (error) return <h1>There has been an error</h1>
+
   return (
     <div className="container">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!!
-      </h1>
+      {comments.map((comment, index) => {
+        return <h1 key={index}>{comment.content}</h1>
+      })}
     </div>
   );
 }
