50362881bf70b4b8e9177f28b25a77b9d3eff763
Charlotte Walker <charlielizzy@me.com>
2022-06-01 18:24:00 +0100

    task 3 complete

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -3,14 +3,17 @@ import React, { useState} from "react"
 const Comment = (props)  => {
 
     const [replyClicked, setReplyClicked] = useState(false);
+    const [reply, setReply] = useState("")
 
 console.log(replyClicked)
 
     return (
         <div>
         <h1 id={`comment-${props.comment.id}`}>{props.comment.content}</h1>
-        <button onClick={() => {setReplyClicked(true)}}>Reply</button>
-        {replyClicked ? (<input type="text"></input>) : null}
+        <button id={`reply-button-${props.comment.id}`} onClick={() => {setReplyClicked(true)}}>Reply</button>
+        {replyClicked ? (<input type="text" value={reply ? `${reply}` : `@${props.comment.user.username}`} 
+        onChange={(e) => {setReply(e.target.value)}}
+         ></input>) : null}
         </div>
     )
 
