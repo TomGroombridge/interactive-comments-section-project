6b6e9624180b12485f03a24d7dfeb93efc816ec0
Carina Druce <trueblue546@gmail.com>
2022-06-08 17:56:54 +0100

    delete modal working

--- src/components/delete.jsx
+++ src/components/delete.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment, useRef } from 'react';
+
+import DeleteModal from './DeleteModal';
+
 
 const Delete = () => {
-  const handleDelete = () => {
-    console.log('deleted');
-  };
-
+  const [open, setOpen] = useState(false);
+  
+  // const handleDelete = () => {
+  //   console.log("deleted")
+  // }
   return (
     <div>
       <button
         className="border-2 border-red-500 text-red-500 p-1 m-1 flex items-center"
-        onClick={() => handleDelete()}
+        onClick={() => setOpen(true)}
       >
         <p>Delete</p>
         <img src="/icons/icon-delete.svg" />
       </button>
+      <DeleteModal setOpen={setOpen} open={open}/>
     </div>
   );
 };
